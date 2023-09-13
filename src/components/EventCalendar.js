2a7000ea002517258f2a124377d770c444a8c11e
Guilherme Klosowski <superguiklos@gmail.com>
2023-09-13 01:28:11 -0300

    Event Click and Hover

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -1,14 +1,54 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import * as bootstrap from 'bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 export default class EventCalendar extends React.Component {
+
   render() {
+    
+    let events = 
+      [
+        {
+          title: 'Evento 1',
+          date: '2023-09-13',
+      
+        },
+        {
+          title: 'Evento 2',
+          date: '2023-09-13',
+      
+        }
+        // other events here
+      ];
+
     return (
-      <FullCalendar
+      <>  
+      <FullCalendar  
         plugins={[ dayGridPlugin ]}
-        defaultView="dayGridMonth"
-      />
+        initialView="dayGridMonth"
+        weekends={true}
+        events={events}
+        // eventDidMount={(info) => {
+        //   return new bootstrap.Popover(info.el, {
+        //     title: info.event.title,
+        //     placement: "auto",
+        //     trigger: "hover",
+        //     customClass: "popoverStyle",
+        //     content:
+        //       "<p>Please subscribe<strong>Bootstrap popover</strong>.</p>",
+        //     html: true,
+        //   });
+        // }}
+        eventClick= {(info) => {
+          alert('Event: ' + info.event.title);
+          alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+          alert('View: ' + info.view.type);
+        }}
+/>
+</>
     )
   }
 }
--- src/pages/CalendarioEsportivo.js
+++ src/pages/CalendarioEsportivo.js
@@ -1,11 +1,17 @@
 import EventCalendar from "../components/EventCalendar";
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 const CalendarioEsportivo = () => {
 
     return(
       <>
 <h1>CalendarioEsportivo</h1>;
-<EventCalendar/>
+<EventCalendar
+  plugins={[ dayGridPlugin ]}
+  initialView="dayGridMonth"
+  weekends={false}
+
+/>
 </>
     ) 
    
